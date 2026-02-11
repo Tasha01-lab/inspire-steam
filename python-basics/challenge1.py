ef5dbc70cab24147ddf89b8515e5357fdba7472c
Tasha01-lab <tashamusau@outlook.com>
2026-02-11 09:16:52 +0200

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,8 @@
+university = "JKUAT"
+grade = "A+"
+favourite_course = "aviation"
+cluster_points = "69"
+print(f"My favourite university is {university}")
+print(f"My grade is {grade}")
+print(f"My favourite course is {favourite_course}")
+print(f"My cluster points are {cluster_points}")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+#This is a comment
+print("Hello from Margie")
+#city / home town
+print("I am from Makueni")
+# my hobbies
+print("I like to draw, watch movies and crotchet")
+
+age = 60
+height = 1.75
+weight = 20
+
+print(f"I am {age} years old")
+print(f"I am also {height} metres tall")
+print(f"And weigh {weight} kgs")
