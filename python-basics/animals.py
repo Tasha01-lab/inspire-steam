92a68daba50729da00497182f87ed024482e54ba
Tasha01-lab <tashamusau@outlook.com>
2026-02-23 10:30:24 +0200

    animals,cars

---
+++ python-basics/animals.py
@@ -0,0 +1,40 @@
+#Name: Tasha Margie Musau
+#Date: 23/02/2026
+
+#Program to show inheritance in python
+
+class Animal():
+
+    def __init__ (self,species,weight,food):
+        self.species = species
+        self.weight = weight
+        self.food = food
+
+    def grow(self,weight):
+        weight = 1.1 * weight
+        print(f"The anmial weight {weight} kgs")
+
+    def eat(self,food):
+        print(f"The animal eats the {food}")
+
+
+class Dog(Animal):
+
+    def __init__ (self,color,height,breed):
+        super().__init__(species,weight,food)
+        self.color = color
+        self.height = height
+        self.breed = breed
+
+    def barks(self):
+        print(f"The dog says woof woof")
+
+class Horse(Animal):
+
+    def __init__ (self,color,weight,food):
+        self.color = color
+        self.weight = weight
+        self.food = food
+
+    def neighs(self):
+        print(f"The horse says neigh neigh")
